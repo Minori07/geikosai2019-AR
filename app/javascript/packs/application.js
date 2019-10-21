6995e49a72ce17adcfe4fa5cc37eb6b6f6115f63
高村美紀 <takamuraminori@koumuramikinoMacBook-Pro.local>
2019-10-21 15:26:40 +0900

    bbb

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,5 +14,3 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-//= require jquery #ない人追加
-//= require serviceworker-companion #追加
