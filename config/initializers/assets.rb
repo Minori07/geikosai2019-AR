9d81ecb843b48b0c0a53ec35edcd48e83d9e6966
高村美紀 <takamuraminori@koumuramikinoMacBook-Pro.local>
2019-10-21 16:05:29 +0900

    aaa

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,3 @@
-Rails.configuration.assets.precompile += %w[serviceworker.js manifest.json]
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
@@ -13,3 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.configuration.assets.precompile += %w[serviceworker.js manifest.json]
