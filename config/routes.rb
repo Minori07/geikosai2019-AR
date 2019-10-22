2e181f88cb59a5f2ce2fc650396186079b331c87
高村美紀 <takamuraminori@koumuramikinoMacBook-Pro.local>
2019-10-22 17:01:35 +0900

    route 修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :stamps
 
   get "home/top" => "home#top"
+  root 'devise/registrations#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   devise_scope :user do
